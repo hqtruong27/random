01992a6ced9e1be7c9d6106f8b9150a46c1a8faf
Truong Hoang <hqtruong27@gmail.com>
2023-12-17 16:23:03 +0700

    update

--- Microservices/Hoyoverse/GenshinImpact/GenshinImpact.Api/Controllers/BannerInfoController.cs
+++ Microservices/Hoyoverse/GenshinImpact/GenshinImpact.Api/Controllers/BannerInfoController.cs
@@ -4,12 +4,12 @@ namespace GenshinImpact.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BannerInfoController(BannerInfoService bannerInfoService) : ControllerBase
+    public class BannerInfoController(IBannerInfoService bannerInfoService) : ControllerBase
     {
         private readonly IBannerInfoService _bannerInfoService = bannerInfoService;
 
         [HttpPost]
-        public async Task<IActionResult> Create(IBannerInfoService request)
+        public async Task<IActionResult> Create(BannerInfoRequest request)
         {
             await _bannerInfoService.CreateAsync(request);
             return Ok();
