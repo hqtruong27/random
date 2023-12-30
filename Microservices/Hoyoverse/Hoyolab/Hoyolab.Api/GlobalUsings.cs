d1bb182eabc8bcc7e4c47b0a701ba0906704564b
Truong Hoang <hqtruong27@gmail.com>
2023-12-30 21:23:05 +0700

    update

--- Microservices/Hoyoverse/Hoyolab/Hoyolab.Api/GlobalUsings.cs
+++ Microservices/Hoyoverse/Hoyolab/Hoyolab.Api/GlobalUsings.cs
@@ -3,6 +3,5 @@ global using Hoyoverse.Infrastructure.Repositories;
 global using Dispatcher;
 global using Dispatcher.Extensions;
 global using Hoyoverse.Infrastructure.Common.Settings;
-global using Hoyoverse.Infrastructure.Core;
 global using Hoyolab.Services.Interfaces;
 global using Hoyolab.Services.Services;
