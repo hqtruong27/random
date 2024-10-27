2b33cf681f173c1495b3306c02880f8e26e2ee70
Truong Hoang <hqtruong27@gmail.com>
2024-10-27 18:22:10 +0700

    update

--- Microservices/Hoyoverse/Hoyolab/Hoyolab.Api/Persistence/MongoDbContext.cs
+++ Microservices/Hoyoverse/Hoyolab/Hoyolab.Api/Persistence/MongoDbContext.cs
@@ -4,6 +4,14 @@ public class MongoDbContext(IMongoDatabase database) : IDatabaseContext
 {
     public IMongoCollection<User> Users => database.GetCollection<User>(nameof(Users));
     public IMongoCollection<Option> Options => database.GetCollection<Option>(nameof(Options));
-    public IMongoCollection<T> Collection<T>() => database.GetCollection<T>(typeof(T).Name);
     public IMongoCollection<T> Collection<T>(string name) => database.GetCollection<T>(name);
+    public IMongoCollection<T> Collection<T>()
+    {
+        return typeof(T).Name switch
+        {
+            nameof(User) => database.GetCollection<T>(nameof(Users)),
+            nameof(Option) => database.GetCollection<T>(nameof(Options)),
+            _ => database.GetCollection<T>($"{typeof(T).Name}s")
+        };
+    }
 }
